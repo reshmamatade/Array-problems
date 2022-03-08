ce396c90d08ae3582e78831e807a4d60115b1451
shubham golhar <sgolhar79@gmail.com>
2022-03-08 17:59:37 +0530

    reduce again

--- reduce.js
+++ reduce.js
@@ -1,8 +1,8 @@
 const students = require("./data/student.json");
-console.log(students);
-let g = students.reduce((acc, items) => (acc = acc + +items.Strength), 0);
-console.log(g);
-let b = students.map((itemsa) => itemsa.Seat).filter((h) => h > 5);
-console.log(b);
-let s = students.map((itemsa) => itemsa.Seat).sort((a, c) => +a - +c);
-console.log(s);
+// console.log(students);
+//let g = students.reduce((acc, items) => (acc = acc + +items.Strength), 0);
+//console.log(g);
+
+// shubham
+let f = students.reduce((acc, itm) => (acc += itm.Name), " ").split("");
+console.log(f);
