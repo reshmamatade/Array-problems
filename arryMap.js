1616ebad739f5075ba583cf032d322e5d0742d71
piyush ghule <piyush.ghule1995@gmail.com>
2022-03-08 17:21:41 +0530

    arry methods-map

---
+++ arryMap.js
@@ -0,0 +1,12 @@
+const students = require("./data/student.json");
+// console.log(students);
+// map value color
+// let a = students.map((items) => items.Color);
+// console.log(a);
+// map value color and sort assending order
+// let b = students.map((items) => items.Color);
+// let c = b.sort();
+// console.log(c);
+// map value of class then sort by assending order
+let d = students.map((items) => items.Class).sort((a, b) => +a - +b);
+console.log(d);
